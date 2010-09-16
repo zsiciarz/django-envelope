142f633e5fd0fc04af04408c49071cd901a510e1
Zbigniew Siciarz <antyqjon@gmail.com>
2010-09-16 12:45:55 +0200

    Version bump for PyPI.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-envelope',
-    version='0.0.1',
+    version='0.1.0',
     description='A contact form app for Django',
     author='Zbigniew Siciarz',
     author_email='antyqjon@gmail.com',
