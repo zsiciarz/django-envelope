cc92b13974d51cad2537c3225df1d9731a9b859d
Zbigniew Siciarz <antyqjon@gmail.com>
2010-09-20 14:07:58 +0200

    Updated the version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-envelope',
-    version='0.1.2',
+    version='0.1.3',
     description='A contact form app for Django',
     author='Zbigniew Siciarz',
     author_email='antyqjon@gmail.com',
