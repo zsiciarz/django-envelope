7877d515f2c915f6a6df81df0c1c2b8904f6dc9d
Zbigniew Siciarz <antyqjon@gmail.com>
2010-09-26 13:20:52 +0200

    Added some more package information and bumped version number.

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='django-envelope',
-    version='0.1.3',
+    version='0.1.4',
     description='A contact form app for Django',
+    long_description=read('README.rst'),
     author='Zbigniew Siciarz',
     author_email='antyqjon@gmail.com',
     url='http://github.com/zsiciarz/django-envelope',
+    download_url='http://pypi.python.org/pypi/django-envelope',
+    license='MIT',
     install_requires=['Django', 'django-honeypot'],
     packages=find_packages(),
     include_package_data=True,
