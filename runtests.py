6239305f24c3fc7a3f438329fef19cf6361f0ca7
Zbigniew Siciarz <antyqjon@gmail.com>
2011-06-02 12:15:41 +0200

    Moved the test settings to runtests.py..

--- runtests.py
+++ runtests.py
@@ -1,10 +1,40 @@
 import os
-import sys
-
-sys.path.insert(0, os.path.abspath('.'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
 
 from django.conf import settings
+
+
+def make_absolute_path(path):
+    return os.path.join(os.path.realpath(os.path.dirname(__file__)), path)
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+            }
+        },
+        INSTALLED_APPS = (
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.messages',
+            'django_nose',
+            'envelope',
+            'honeypot',
+        ),
+        TEMPLATE_DIRS = (
+            make_absolute_path('tests/templates'),
+        ),
+        ROOT_URLCONF = 'example_project.urls',
+        TEST_RUNNER = 'django_nose.NoseTestSuiteRunner',
+        NOSE_ARGS = ['--stop'],
+        HONEYPOT_FIELD_NAME = 'email2',
+    )
+
+
 from django.core.management import call_command
 
 call_command('test', 'envelope')
