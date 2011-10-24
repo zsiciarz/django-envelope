95e774120a6e3f140cada20dc954a8a342c836a5
Zbigniew Siciarz <antyqjon@gmail.com>
2011-10-24 23:25:59 +0200

    Explicitly state 1.3 dependency.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url='http://github.com/zsiciarz/django-envelope',
     download_url='http://pypi.python.org/pypi/django-envelope',
     license='MIT',
-    install_requires=['Django', 'django-honeypot'],
+    install_requires=['Django>=1.3', 'django-honeypot'],
     packages=find_packages(),
     include_package_data=True,
     classifiers=['Development Status :: 5 - Production/Stable',
