e023a39d92833636731ff97c3130cc868870d08f
Zbigniew Siciarz <antyqjon@gmail.com>
2011-11-05 23:02:20 +0100

    Unit tests for honeypot spam filter.

--- envelope/tests/__init__.py
+++ envelope/tests/__init__.py
@@ -1,2 +1,3 @@
 from .forms import BaseContactFormTestCase, ContactFormTestCase
 from .views import ContactViewTestCase
+from .spam_filters import CheckHoneypotTestCase
---
+++ envelope/tests/spam_filters.py
@@ -0,0 +1,45 @@
+u"""
+Unit tests for spam filters.
+"""
+
+from django.conf import settings
+from django.test import TestCase
+
+from envelope.spam_filters import check_honeypot
+
+
+# mocking form and request, no need to use the real things here
+class FakeForm(object):
+    pass
+
+
+class FakeRequest(object):
+    def __init__(self):
+        self.method = 'POST'
+        self.POST = {}
+
+
+class CheckHoneypotTestCase(TestCase):
+    u"""
+    Unit tests for ``check_honeypot`` spam filter.
+    """
+
+    def setUp(self):
+
+        self.form = FakeForm()
+        self.request = FakeRequest()
+        self.honeypot = getattr(settings, 'HONEYPOT_FIELD_NAME', 'email2')
+
+    def test_empty_honeypot(self):
+        u"""
+        Empty honeypot field is a valid situation.
+        """
+        self.request.POST[self.honeypot] = u''
+        self.assertTrue(check_honeypot(self.request, self.form))
+
+    def test_filled_honeypot(self):
+        u"""
+        A value in the honeypot field is an indicator of a bot request.
+        """
+        self.request.POST[self.honeypot] = u'Hi, this is a bot'
+        self.assertFalse(check_honeypot(self.request, self.form))
