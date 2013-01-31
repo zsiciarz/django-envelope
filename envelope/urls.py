b5e746c67a178b5cf04871df9e4f46e67c7aa96d
Anders Petersson <me@anderspetersson.se>
2013-01-31 16:48:06 +0100

    Use a try/except for the new url import path to be backwards compatible with django before 1.6.

--- envelope/urls.py
+++ envelope/urls.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-
-from django.conf.urls import patterns, url
+try:
+	# Django 1.5 and below.
+    from django.conf.urls.defaults import patterns, url
+except ImportError:
+	# Django 1.6+
+    from django.conf.urls import patterns, url
 
 from envelope.views import ContactView
 
