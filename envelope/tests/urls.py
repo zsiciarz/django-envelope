c6a44636914d9409071ca84160e7631acda55877
Zbigniew Siciarz <antyqjon@gmail.com>
2013-02-02 20:08:31 +0100

    Fixed ImportError when running tests with Django 1.6 trunk.

--- envelope/tests/urls.py
+++ envelope/tests/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls.defaults import include, patterns, url
+try:
+    # Django 1.5 and below.
+    from django.conf.urls.defaults import patterns, include, url
+except ImportError:
+    # Django 1.6+
+    from django.conf.urls import patterns, include, url
 
 from envelope.views import ContactView
 
