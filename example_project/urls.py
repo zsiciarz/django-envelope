a919ebde014e5ad08c514c3edd1d0ce21e1eb250
Zbigniew Siciarz <antyqjon@gmail.com>
2013-05-17 22:10:34 +0200

    Fixed NameError in example_project.

--- example_project/urls.py
+++ example_project/urls.py
@@ -1,9 +1,9 @@
 try:
     # Django 1.5 and below.
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import patterns, include
 except ImportError:
     # Django 1.6+
-    from django.conf.urls import patterns, url
+    from django.conf.urls import patterns, include
 
 urlpatterns = patterns('',
     (r'', include('envelope.urls')),
