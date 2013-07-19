40c02d708b79c428e1fe2d05d314b060bde3c98c
Zbigniew Siciarz <antyqjon@gmail.com>
2013-07-19 21:53:16 +0200

    Put new-style URLconf imports first.

--- envelope/tests/urls.py
+++ envelope/tests/urls.py
@@ -1,9 +1,9 @@
 try:
-    # Django 1.5 and below.
-    from django.conf.urls.defaults import patterns, include, url
-except ImportError:
     # Django 1.6+
     from django.conf.urls import patterns, include, url
+except ImportError:
+    # Django 1.4 and 1.5
+    from django.conf.urls.defaults import patterns, include, url
 
 from envelope.views import ContactView
 
--- example_project/urls.py
+++ example_project/urls.py
@@ -1,9 +1,9 @@
 try:
-    # Django 1.5 and below.
-    from django.conf.urls.defaults import patterns, include
-except ImportError:
     # Django 1.6+
     from django.conf.urls import patterns, include
+except ImportError:
+    # Django 1.4 and 1.5
+    from django.conf.urls.defaults import patterns, include
 
 urlpatterns = patterns('',
     (r'', include('envelope.urls')),
