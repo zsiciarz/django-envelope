fcdaea180c239845f247fffaefabacd7d5df3f4e
Zbigniew Siciarz <antyqjon@gmail.com>
2014-01-04 10:12:39 +0100

    2014

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'django-envelope'
-copyright = u'2011-2013, Zbigniew Siciarz'
+copyright = u'2011-2014, Zbigniew Siciarz'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
