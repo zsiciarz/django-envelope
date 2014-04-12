80151a90ce5e098a22d9a4f1cdfa8fbf594d4e32
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-12 19:31:11 +0200

    Removed unused settings.

--- envelope/settings.py
+++ envelope/settings.py
@@ -10,16 +10,7 @@ from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 
-DEFAULT_CONTACT_CHOICES = (
-    ('', _("Choose")),
-    (10, _("A general question regarding the website")),
-    (None, _("Other")),
-)
-
 FROM_EMAIL = settings.DEFAULT_FROM_EMAIL
-
-CONTACT_CHOICES = getattr(settings, 'ENVELOPE_CONTACT_CHOICES',
-                          DEFAULT_CONTACT_CHOICES)
 
 EMAIL_RECIPIENTS = getattr(settings, 'ENVELOPE_EMAIL_RECIPIENTS',
                            [settings.DEFAULT_FROM_EMAIL])
