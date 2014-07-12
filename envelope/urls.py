f55c42dd184d142569bbd3443b5fa8c0a5e532c7
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-12 08:20:29 +0200

    Removed Django 1.4 mentions from code.

--- envelope/urls.py
+++ envelope/urls.py
@@ -4,7 +4,7 @@ try:
     # Django 1.6+
     from django.conf.urls import patterns, url
 except ImportError:  # pragma: no cover
-    # Django 1.4 and 1.5
+    # Django 1.5
     from django.conf.urls.defaults import patterns, url
 
 from envelope.views import ContactView
--- example_project/urls.py
+++ example_project/urls.py
@@ -2,7 +2,7 @@ try:
     # Django 1.6+
     from django.conf.urls import patterns, include
 except ImportError:
-    # Django 1.4 and 1.5
+    # Django 1.5
     from django.conf.urls.defaults import patterns, include
 
 urlpatterns = patterns('',
