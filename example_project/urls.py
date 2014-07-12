63f3cd15a286cf8948f8ae81d081d3c1493fcdc0
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-12 21:46:31 +0200

    Added example with messages mixin.

--- example_project/urls.py
+++ example_project/urls.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 try:
     # Django 1.6+
     from django.conf.urls import patterns, include, url
@@ -5,9 +7,20 @@ except ImportError:
     # Django 1.5
     from django.conf.urls.defaults import patterns, include, url
 
+
+from braces.views import FormMessagesMixin
+
 from envelope.views import ContactView
+
+
+class MessagesContactView(FormMessagesMixin, ContactView):
+    form_invalid_message = "There was en error in the contact form."
+    form_valid_message = "Thank you for your message."
+    template_name = "envelope/messages_contact.html"
+
 
 urlpatterns = patterns('',
     url(r'', include('envelope.urls')),
     url(r'^crispy/', ContactView.as_view(template_name='envelope/crispy_contact.html'), name='crispy-contact'),
+    url(r'^messages/', MessagesContactView.as_view(), name='messages-contact'),
 )
