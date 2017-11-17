b4a1dc63d18a706483897900f8e4b3287eb77a89
Zbigniew Siciarz <zbigniew@siciarz.net>
2017-11-17 21:02:34 +0100

    Import reverse from django.urls.

--- tests/test_views.py
+++ tests/test_views.py
@@ -10,7 +10,7 @@ import unittest
 
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.test import TestCase
 
 try:
